9e3b5de389b97ce37f2000138b074b008c48e021
Siddharth Mudgal <siddharthmudgal@Siddharths-MacBook-Pro-2.local>
2021-10-09 14:42:36 +0800

    ADDED user entity and rest

---
+++ src/main/java/com/dealstop/mvcEcommerce/configurations/PersistenceConfigurations.java
@@ -0,0 +1,12 @@
+package com.dealstop.mvcEcommerce.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaAuditing
+public class PersistenceConfigurations {
+}
